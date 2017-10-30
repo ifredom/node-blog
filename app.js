0220feb3a3d55d87ca6b9dabba01ae84f0253327
ifredom <1950735817@qq.com>
2017-10-30 18:22:46 +0800

    优化基础中间件结构

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session'); //my add
+var MongoStore = require('connect-mongo')(session); //my add
+var flash = require('connect-flash'); //my add
+var winston = require('winston'); //my add
+var expressWinston = require('express-winston'); //my add
+var routes = require('./routes'); //my add
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var pkg = require('./package');
+// var config = require('config-lite')(__dirname);
+var config = require('./config/default');
 var app = express();
 
 // view engine setup
@@ -18,29 +26,90 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
+// 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// ============self add start
+// session 中间件
+app.use(session({
+    name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
+    secret: config.session.secret, // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+    resave: true, // 强制更新 session
+    saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
+    cookie: {
+        maxAge: config.session.maxAge // 过期时间，过期后 cookie 中的 session id 自动删除
+    },
+    store: new MongoStore({ // 将 session 存储到 mongodb
+        url: config.mongodb // mongodb 地址
+    })
+}));
+// flash 中间件，用来显示通知
+app.use(flash());
+
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+    uploadDir: path.join(__dirname, 'public/image'), // 上传文件目录
+    keepExtensions: true // 保留后缀
+}));
+
+// 设置模板全局常量
+app.locals.blog = {
+    title: pkg.name,
+    description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+    // res.locals.user = req.session.user;
+    res.locals.success = req.flash('success').toString();
+    res.locals.error = req.flash('error').toString();
+    next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// 正常请求的日志
+app.use(expressWinston.logger({
+    transports: [
+        new(winston.transports.Console)({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/success.log'
+        })
+    ]
+}));
+// ============self add end
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 路由
+routes(app);
+
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/error.log'
+        })
+    ]
+}));
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express')
-var router = express.Router()
+// var express = require('express')
+// var router = express.Router()
 
-/* GET home page. */
-router.get('/', (req, res, next)=> {
-  res.render('index', { title: 'Express-title-5' })
-});
+// var checkNotLogin = require('../middlewares/check').checkNotLogin
+// /* GET home page. */
+// router.get('/', checkNotLogin, (req, res, next)=> {
+//   res.render('index', { title: 'Express-title-5' })
+// });
 
-module.exports = router
+// module.exports = router
+
+
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.render('index', { title: 'Express-title-5' })
+    });
+    // app.use('/signup', require('./signup'));
+    // app.use('/signin', require('./signin'));
+    // app.use('/signout', require('./signout'));
+    // app.use('/posts', require('./posts'));
+  
+    // 404 page
+    app.use(function (req, res) {
+      if (!res.headersSent) {
+        res.status(404).render('404');
+      }
+    });
+  };
+  
