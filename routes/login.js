c140791dc25be702467ac3e9bd04e3b9b98dce4d
ifredom <1950735817@qq.com>
2017-11-01 09:52:12 +0800

    添加接口处理

--- routes/login.js
+++ routes/login.js
@@ -8,13 +8,27 @@ var checkNotLogin = require('../middlewares/check').checkNotLogin;
 router.get('/', (req, res, next) =>{
     res.render('login', { title: 'Express-Node-Blog' })
 });
+router.get('/:min/:max', (req, res, next) => {
+    var min = parseInt(req.params.min);
+    var max = parseInt(req.params.max);
+    console.log(min, max)
+    if (isNaN(min) || isNaN(max)) {
+        res.status(400);
+        res.json({ error: "Bad request." });
+        return;
+    }
+
+    var result = Math.round((Math.random() * (max - min)) + min);
+    res.json({ result: result });
+    // res.render('login', { title: 'hehe' })
+});
 // POST /signin 用户登录
 router.post('/', function (req, res, next) {
-    console.log(req)
+    
     var name = req.fields.name;
     var password = req.fields.password;
     console.log(name)
     console.log(password)
-    res.render('login', { title: 'post' });
+    res.redirect(`/posts`);
 });
 module.exports = router;
