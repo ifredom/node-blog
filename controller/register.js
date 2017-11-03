2d989e39ac867d07cae49441391598b28d898927
yuwanfeng <1950735817@qq.com>
2017-11-03 23:19:25 +0800

    add register

--- controller/register.js
+++ controller/register.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var adminModel = require('../models/admin/admin.js')
 
 /* GET /register. */
 router.get('/', function(req, res, next) {
     res.render('register');
 });
+
+/* POST /register. */
+router.post('/', function(req, res) {
+    var name = req.body.name
+    var password = req.body.password
+
+    var admin = new adminModel({ name: 'ifredom', password: '111111' })
+    admin.save(function(err) {
+        if (err) {
+            console.log(err)
+        }
+        // saved success!
+        res.redirect('login');
+    })
+})
 
 module.exports = router;
--- mongodb/mongodb.js
+++ mongodb/mongodb.js
@@ -2,26 +2,26 @@
 var mongoose = require('mongoose')
 var config = require('../config')
 mongoose.connect(config.mongodb, {
-  useMongoClient: true
+    useMongoClient: true
 });
 mongoose.Promise = global.Promise;
 
 var db = mongoose.connection;
 
-db.once('open', function () {
-  console.log("数据库链接成功了！")
+db.once('open', function() {
+    console.log("数据库链接成功了！\n")
 })
 
-db.on('error', function (error) {
-  console.log('链接数据库时出错了！' + error)
-  mongoose.disconnect();
+db.on('error', function(error) {
+    console.log('链接数据库时出错了！请先安装monggodb或者开启mongodb服务!\n' + error)
+    mongoose.disconnect();
 })
 
-db.on('close', function () {
-  console.log('数据库断开，重新连接数据库')
-  mongoose.connect(config.mongodb, {
-    useMongoClient: true
-  });
+db.on('close', function() {
+    console.log('数据库断开，重新连接数据库')
+    mongoose.connect(config.mongodb, {
+        useMongoClient: true
+    });
 })
 
 module.exports = mongoose
